0a25c1503c0129f111e911d41a054c551d579da0
xialuchao <shylock_xia@126.com>
2020-04-17 18:26:29 +0800

    add hr api

--- demo/debugtalk.py
+++ demo/debugtalk.py
@@ -1,5 +1,14 @@
 
 import time
+import cx_Oracle
 
 def sleep(n_secs):
     time.sleep(n_secs)
+
+def get_quota():
+    return 997600
+    # conn = cx_Oracle.connect('zhao/zhao@192.168.0.66:1521/zhaotest')
+    # cursor = conn.cursor()
+    # sql = "select quota from users where id = (:1)"
+    # cursor.execute(sql, ([username]))
+
