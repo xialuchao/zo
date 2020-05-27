090f46c20a7d1926e22bcc16666fd1819925d0a8
xialuchao <shylock_xia@126.com>
2020-05-27 17:24:11 +0800

    + selenium

--- appium_online_9/web/driver/Client.py
+++ appium_online_9/web/driver/Client.py
@@ -2,6 +2,8 @@
 # from appium.webdriver.webdriver import WebDriver
 from selenium import webdriver
 import yaml
+from selenium.webdriver import DesiredCapabilities
+
 
 class SeleniumClient(object):
 
@@ -21,6 +23,8 @@ class SeleniumClient(object):
         # cls.driver = webdriver.Remote(desired_capabilities=DesiredCapabilities.CHROME)
         if not cls.driver_exists():
             cls.driver = webdriver.Chrome("E:\\chromedriver_win32\\chromedriver.exe")
+            # cls.driver = webdriver.Remote(command_executor='http://192.168.6.90:4444/wd/hub',
+            #                                desired_capabilities=DesiredCapabilities.CHROME)
         cls.driver.get("http://ft1.sh.zhaoonline.com")
 
         # cls.driver = webdriver.Chrome("/Users/shylock/github/zo/appium_online_9/chromedriver")
